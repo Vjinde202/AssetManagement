dc16945c6daeab56ba2019a31156928e83de6750
VamshiCapg <vamshiadn@gmail.com>
2022-07-18 21:38:17 +0530

    First

---
+++ src/test/java/com/abc/AssetManagement/AssetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.AssetManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
